a4d75ad110e2431d64ce5a7d2031a996348097a2
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-28 21:54:19 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,29 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+     :montague => {
+       :patiarch => {
+         :name => "Lord Montague",
+         :age => 53 
+       },
+       :matriarch => {
+         :name => "Lady MOntague",
+         :age => 54
+       },
+       :hero =>{
+         :name => "Romeo",
+         :age => 15,
+         :status => "alive"
+       },
+       :hero_friends => [] 
+     },
+     :capulet => {
+       :partiarch => {
+         :name => "Lord Capulet",
+         :
+       },
+       :matriarch => {},
+       :heroine => {},
+       :heroine_friends => []
+     }
+   }
 end
